3433fe472bd0461492e7d98db37a736f931a09a9
Adriano Molin <adriano.molin@hotmail.com>
2024-03-22 11:10:01 -0300

    Fix vite react config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     react({
-      include: '**/*.tsx',
+      include: /\.(mdx|js|jsx|ts|tsx)$/,
     }),
     tsconfigPaths()
   ],
